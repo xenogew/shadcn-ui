d4340007be282966060164ed64aa39e324f8b3d2
Natta Wang <xenogew@gmail.com>
2025-05-12 00:24:13 +0900

    first draft component with Button

---
+++ packages/dioxus/button/src/new_york.rs
@@ -0,0 +1,135 @@
+use dioxus::prelude::*;
+use tailwind_fuse::*;
+
+#[derive(TwClass)]
+#[tw(
+    class = "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+)]
+pub struct ButtonClass {
+    pub variant: ButtonVariant,
+    pub size: ButtonSize,
+}
+
+#[derive(PartialEq, TwVariant)]
+pub enum ButtonVariant {
+    #[tw(
+        default,
+        class = "bg-primary text-primary-foreground shadow hover:bg-primary/90"
+    )]
+    Default,
+    #[tw(class = "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90")]
+    Destructive,
+    #[tw(
+        class = "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground"
+    )]
+    Outline,
+    #[tw(class = "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80")]
+    Secondary,
+    #[tw(class = "hover:bg-accent hover:text-accent-foreground")]
+    Ghost,
+    #[tw(class = "text-primary underline-offset-4 hover:underline")]
+    Link,
+}
+
+#[derive(PartialEq, TwVariant)]
+pub enum ButtonSize {
+    #[tw(default, class = "h-9 px-4 py-2")]
+    Default,
+    #[tw(class = "h-8 rounded-md px-3 text-xs")]
+    Sm,
+    #[tw(class = "h-10 rounded-md px-8")]
+    Lg,
+    #[tw(class = "h-9 w-9")]
+    Icon,
+}
+
+#[derive(PartialEq, Props, Clone)]
+pub struct ButtonProps {
+    #[props(default)]
+    pub variant: ButtonVariant,
+    #[props(default)]
+    pub size: ButtonSize,
+
+    // Global attributes
+    #[props(default)]
+    pub autofocus: bool,
+    #[props(default)]
+    pub class: Option<String>,
+    #[props(default)]
+    pub id: Option<String>,
+    #[props(default)]
+    pub style: String,
+
+    // Attributes from `button`
+    #[props(default)]
+    pub command: Option<String>,
+    #[props(default)]
+    pub commandfor: Option<String>,
+    #[props(default)]
+    pub disabled: bool,
+    #[props(default)]
+    pub form: Option<String>,
+    #[props(default)]
+    pub formaction: Option<String>,
+    #[props(default)]
+    pub formenctype: Option<String>,
+    #[props(default)]
+    pub formmethod: Option<String>,
+    #[props(default)]
+    pub formnovalidate: bool,
+    #[props(default)]
+    pub formtarget: Option<String>,
+    #[props(default)]
+    pub name: Option<String>,
+    #[props(default)]
+    pub popovertarget: Option<String>,
+    #[props(default)]
+    pub popovertargetaction: Option<String>,
+    #[props(default)]
+    pub r#type: Option<String>,
+    #[props(default)]
+    pub value: Option<String>,
+
+    // Event handler attributes
+    #[props(default)]
+    pub onclick: Option<EventHandler<MouseEvent>>,
+    
+
+    #[props(default)]
+    pub as_child: bool,
+    #[props(default)]
+    pub children: Element,
+}
+
+
+#[component]
+pub fn Button(props: &'static ButtonProps) -> Element {
+    let class = ButtonClass {
+        variant: props.variant,
+        size: props.size,
+    }
+    .with_class(
+        props.class.clone().unwrap_or_default(),
+    );
+
+    if props.as_child {
+        return props.children.clone();
+    }
+
+    rsx! {
+        button {
+            autofocus: props.autofocus,
+            class: "{class}",
+            id: props.id.clone(),
+            style: "{props.style}",
+            disabled: props.disabled,
+            r#type: props.r#type.clone(),
+            onclick: move |evt| {
+                if let Some(handler) = &props.onclick {
+                    handler.call(evt);
+                }
+            },
+            {props.children.clone()}
+        }
+    }
+}
